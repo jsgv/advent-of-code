20959f8fdf6aea385c99e64bd03d14fddacce8a9
Jesus <j@pseudo.cc>
2020-12-16 19:23:27 -0800

    Day 8

---
+++ 08/README.md
@@ -0,0 +1,3 @@
+# Day 8
+
+https://adventofcode.com/2020/day/8
---
+++ 08/handheld-halting.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type I struct {
+	operation string
+	value     int
+	count     int
+}
+
+var (
+	instructions []*I
+	actionRx     = regexp.MustCompile(`([a-z]{3}) ([-|+]?)(\d+)`)
+)
+
+func work(step int, instructions []*I, acc int) int {
+	if len(instructions) <= step {
+		fmt.Println("Part 2:", acc)
+		return acc
+	}
+
+	i := instructions[step]
+
+	if i.count > 0 {
+		return acc
+	}
+
+	i.count += 1
+
+	switch i.operation {
+	case "nop":
+		return work(step+1, instructions, acc)
+	case "acc":
+		acc += i.value
+		return work(step+1, instructions, acc)
+	case "jmp":
+		return work(step+i.value, instructions, acc)
+	default:
+		fmt.Println("need to implement:", i.operation)
+	}
+
+	return acc
+}
+
+func main() {
+	b, _ := ioutil.ReadFile("input.txt")
+	r := strings.Split(string(b), "\n")
+
+	for _, v := range r {
+		if v == "" {
+			continue
+		}
+
+		parts := actionRx.FindStringSubmatch(v)
+		value, _ := strconv.Atoi(parts[2] + parts[3])
+
+		instructions = append(instructions, &I{
+			operation: parts[1],
+			value:     value,
+			count:     0,
+		})
+	}
+
+	fmt.Println("Part 1:", work(0, sliceCopy(), 0))
+
+	for idx, v := range instructions {
+		switch v.operation {
+		case "nop":
+			c := sliceCopy()
+			c[idx].operation = "jmp"
+			if c[idx].value == 0 {
+				c[idx].value = 1
+			}
+
+			work(0, c, 0)
+			break
+		case "jmp":
+			c := sliceCopy()
+			c[idx].operation = "nop"
+			work(0, c, 0)
+			break
+		}
+	}
+}
+
+func sliceCopy() []*I {
+	c := make([]*I, len(instructions))
+
+	for i, p := range instructions {
+		if p == nil {
+			continue
+		}
+
+		v := *p
+
+		c[i] = &v
+	}
+
+	return c
+}
